dd3ea514a779e21a77f6a5fc9f73c22e89eefa92
ksmsm99 <sm990717@thesyso.com>
2022-07-29 16:23:09 +0900

    1

--- demo/demo.service/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo.service/src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class DemoServiceApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoServiceApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
